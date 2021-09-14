3259a545a8b0f68a8a01332863770b9b5d4ba623
arikurniawans <ari.curnia.one@gmail.com>
2021-09-14 21:53:40 +0700

    Perbaikan Menu estimasi

--- app/src/main/java/com/example/skapanhidro/MenuApps.java
+++ app/src/main/java/com/example/skapanhidro/MenuApps.java
@@ -8,13 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MenuApps extends AppCompatActivity {
-CardView cardPompa,cardSensor;
+CardView cardPompa,cardSensor, cardestimasi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_apps);
         cardPompa = (CardView) findViewById(R.id.cardPompa);
         cardSensor= (CardView) findViewById(R.id.cardSensor);
+        cardestimasi = (CardView) findViewById(R.id.cardestimasi);
 
         cardPompa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +30,14 @@ CardView cardPompa,cardSensor;
             public void onClick(View v) {
                 Intent sensor = new Intent(MenuApps.this, SensorActivity.class);
                 startActivity(sensor);
+            }
+        });
+
+        cardSensor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent estimasi = new Intent(MenuApps.this, EstimasiActivity.class);
+                startActivity(estimasi);
             }
         });
     }
